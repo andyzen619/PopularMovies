987aa8eb1fb8fc37cb7e46c7715b51a817d2d2a2
andyzen619 <andyzen619@gmail.com>
2016-12-04 22:36:36 -0500

    set up basic layouts

---
+++ app/src/main/java/com/example/andydesk/popularmovies/MovieGridFragment.java
@@ -0,0 +1,38 @@
+package com.example.andydesk.popularmovies;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MovieGridFragment extends Fragment {
+
+    public MovieGridFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.fragment_movie_grid, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.main_menu, menu);
+    }
+}
